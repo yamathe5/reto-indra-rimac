126efa2b747e8dc7cb4976fb3b26e7a92c7aff79
yamathe <manuelsegmar@gmail.com>
2022-08-27 19:59:46 -0500

    ADD: Sidebar (ArmaTuPlan Component)

---
+++ src/components/ArmaTuPlanComponents/Sidebar/SidebarItem.js
@@ -0,0 +1,28 @@
+export default function SidebarItem (){
+
+  function handleLogout(){
+
+  }
+
+  return (
+    <>
+      <div className="sidebar--mobile">
+        <button onClick={handleLogout} className="rounded-button__red">	&#x3C;</button>
+        <p className="text-xs font-700" >PASO 2 DE 2</p>
+        <div className="pruple-bar"></div>
+      </div>
+      
+      <div className="sidebar--desktop">
+        <div>
+          <button className="rounded-button" >1</button>
+          <p className="text-lg font-400" >Datos</p>
+        </div>
+        <div>
+          <button className="rounded-button__selected">2</button>
+          <p className="text-lg font-400" >Arma tu plan</p>
+        </div>
+        
+      </div>
+    </>
+  )
+}
